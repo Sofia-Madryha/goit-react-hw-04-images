a6a161a803fc1d325faffcce77f797faea2fe68f
Sofia-Madryha <sonyamadryha3@gmail.com>
2023-11-13 22:44:56 +0200

    hide loadmore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPhoto } from './api';
 import { SearchBar } from './SearchBar/SearchBar';
 import { Gallery } from './Gallery/Gallery';
@@ -6,68 +6,58 @@ import { Button } from './Button/Button';
 
 import { Loader } from './Loader/Loader';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    loading: false,
-    error: false,
-    loadMore: false,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
 
-  handleSubmit = evt => {
+  useEffect(() => {
+    if (!query) {
+      return;
+    } else {
+      const getImages = async () => {
+        try {
+          setLoading(true);
+          const search = await fetchPhoto(query, page);
+
+          setImages(prevState =>
+            page === 1 ? search.hits : [...prevState, ...search.hits]
+          );
+          setLoading(false);
+          setLoadMore(page < Math.ceil(search.totalHits / 12));
+        } catch (error) {
+          setError(true);
+        } finally {
+          setLoading(false);
+        }
+      };
+      getImages();
+    }
+  }, [query, page]);
+
+  const handleSubmit = evt => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const searchValue = form.elements.query.value;
-    this.setState({
-      query: searchValue,
-      images: [],
-      page: 1,
-    });
+    setQuery(searchValue);
+    setImages([]);
+    setPage(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    try {
-      if (
-        prevState.query !== this.state.query ||
-        this.state.page !== prevState.page
-      ) {
-        this.setState({ loading: true });
-        const search = await fetchPhoto(this.state.query, this.state.page);
-        this.setState({
-          images:
-            this.state.page === 1
-              ? search.hits
-              : [...prevState.images, ...search.hits],
-          loading: false,
-          // loadMore: this.state.page < Math.ceil(search.totalHits / 12)
-        });
-      }
-    } catch (error) {
-      // this.setState({ error: true });
-    } finally {
-      // this.setState({ loading: false });
-    }
-  }
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
 
-  render() {
-    return (
-      <div>
-        <SearchBar onSubmit={this.handleSubmit} />
-
-        {this.state.images.length > 0 && <Gallery items={this.state.images} />}
-        {this.state.loading && <Loader></Loader>}
-        {/* {this.state.images.length > 0 && <div>GALLERY</div>}
-        {this.state.loading && <div>Loader...</div>} */}
-        {this.state.images.length > 0 && (
-          <Button onClick={this.handleLoadMore}></Button>
-        )}
-      </div>
-    );
-  }
-}
+      {images.length > 0 && <Gallery items={images} />}
+      {loading && <Loader></Loader>}
+      {loadMore && <Button onClick={handleLoadMore}></Button>}
+    </div>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { Modal } from 'components/Modal/Modal';
-import "../ImageGalleryItem/ImageGalleryItem.css"
+import '../ImageGalleryItem/ImageGalleryItem.css';
 
 const customStyles = {
   content: {
@@ -11,55 +11,51 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    padding:'0'  },
+    padding: '0',
+  },
   overlay: {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-    zIndex: "1200",
-  }
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    zIndex: '1200',
+  },
 };
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
-  };
-  openModal = () => {
-    if (!this.state.isModalOpen) {
-      this.setState({ isModalOpen: true });
-    }
+export const ImageGalleryItem = 
+({image:{ id, webformatURL, largeImageURL }}) => {
+const [isModalOpen, setIsModalOpen]= useState(false)
+
+  const openModal = () => {
+   setIsModalOpen(true)
   };
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
+ const  closeModal = () => {
+    setIsModalOpen(false)
   };
 
-  render() {
-    const { id, webformatURL, largeImageURL } = this.props.image;
-    return (
-      <li className="image-gallery-item">
-        <img
-          className="image-gallery-item-image"
-          src={webformatURL}
-          alt={id}
-          loading="lazy"
-          onClick={this.openModal}
-        />
+  return (
+    <div key={id} className="image-gallery-item">
+      <img
+        className="image-gallery-item-image"
+        src={webformatURL}
+        alt={id}
+        loading="lazy"
+        onClick={openModal}
+      />
 
-        <Modal
-          isOpen={this.state.isModalOpen}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel="Image Modal"
-          largeImageURL={largeImageURL}
-        />
-      </li>
-    );
-  }
-}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Image Modal"
+        largeImageURL={largeImageURL}
+      />
+    </div>
+  );
+};
